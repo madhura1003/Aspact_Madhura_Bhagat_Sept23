dae9af159f0422d04fe4c9d421ab85ea9a570427
madhura1003 <145961073+madhura1003@users.noreply.github.com>
2023-10-05 22:28:08 +0530

    Add files via upload

---
+++ PATT8_4.C
@@ -0,0 +1,22 @@
+main()
+{
+ int i,j;
+ clrscr();
+ for(i='A';i<='E';i++)
+ {
+  for(j='E';j>='A';j--)
+  {
+   printf(" %c ",j);
+  }
+  printf("\n");
+  }
+
+getch();
+}
+/*
+EDCBA
+EDCBA
+EDCBA
+EDCBA
+EDCBA
+*/
