2f53590c2427bbc5e0d5240e38cbfbef35fdded5
madhura1003 <145961073+madhura1003@users.noreply.github.com>
2023-10-07 22:58:29 +0530

    Add files via upload

---
+++ PATT3_5.C
@@ -0,0 +1,23 @@
+main()
+{
+ int i,j;
+ clrscr();
+ for(i=5;i>=1;i--)
+ {
+  for(j=1;j<=i;j++)
+  {
+   printf(" %d ",i);
+  }
+  printf("\n");
+
+ }
+ getch();
+}
+
+/*
+55555
+4444
+333
+22
+1
+*/
